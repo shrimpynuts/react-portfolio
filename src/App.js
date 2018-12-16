db2e962fa4078c6c00c7b3b8394e2e550e45e375
Jonathan Cai <caimjonathan@gmail.com>
2018-12-16 01:11:39 -0800

    initial changes, headshot img, para

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <img src={require("./head.jpeg")} width={300} height={300} />
+      <p>
+      Hi there! I'm Jonathan
+      </p>
+      {/* <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a> */}
       </div>
     );
   }
