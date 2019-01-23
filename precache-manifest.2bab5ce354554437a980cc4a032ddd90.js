8cb2a5342005d5d7f5ad81f70aa58f3de871d3e2
Jonathan Cai <caimjonathan@gmail.com>
2019-01-23 00:39:45 -0600

    Updates

---
+++ precache-manifest.2bab5ce354554437a980cc4a032ddd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1332fe9a3c2dfcd5f5320e4388009b05",
+    "url": "/static/media/head.1332fe9a.jpeg"
+  },
+  {
+    "revision": "1b15bee2870f566f0508d1723f1a209e",
+    "url": "/static/media/Jonathan-Cai-Resume.1b15bee2.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c18cb0b46b73eda6468b",
+    "url": "/static/js/main.c18cb0b4.chunk.js"
+  },
+  {
+    "revision": "f5ad964d00fd963abe5c",
+    "url": "/static/js/1.f5ad964d.chunk.js"
+  },
+  {
+    "revision": "c18cb0b46b73eda6468b",
+    "url": "/static/css/main.d863f5fd.chunk.css"
+  },
+  {
+    "revision": "5913aad2309b82f9b14e5c2a0ec71db1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e264b260604783de541c33ec3efed16.js"
+  "/precache-manifest.2bab5ce354554437a980cc4a032ddd90.js"
 );
 
 workbox.clientsClaim();
